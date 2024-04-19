f4c337aa9fc0655f20c7c12cb1e61f2949d61990
ibrahimovic <144237765+ibrahims09@users.noreply.github.com>
2024-04-19 13:37:09 +0200

    fixade culmner

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,23 @@
-import './App.css'
+import './App.css';
 import Home from './components/main/Home';
 import Login from './components/login/Login';
 import About from './components/about/About';
+import TodoColumn from './components/TodoColumn'; 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
-
   return (
     <div id="AppMainContainer">
       <BrowserRouter>
         <Routes>
           <Route path='/login' element={<Login />} />
           <Route path="/about" element={<About />} />
+          <Route path='/todo' element={<TodoColumn />} /> {}
           <Route path='/' element={<Home />} />
         </Routes>
       </BrowserRouter>
     </div>
-  )
+  );
 }
 
 export default App;
---
+++ src/components/TodoColumn.jsx
@@ -0,0 +1,19 @@
+// TodoColumn.jsx
+import React from 'react';
+
+const TodoColumn = () => {
+    return (
+        <div className="todo-column">
+            {}
+            <h2>Todo</h2>
+            {}
+            <ul>
+                <li>Todo 1</li>
+                <li>Todo 2</li>
+                <li>Todo 3</li>
+            </ul>
+        </div>
+    );
+};
+
+export default TodoColumn;
